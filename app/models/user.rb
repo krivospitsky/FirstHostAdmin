1edee091b8203058e3bafcd8eb227bc152dc13c9
alex krivospitsky <alex@krivospitsky.ru>
2012-09-12 16:10:57 +0400

    наполняю

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name
   before_create :do_create
+  before_destroy :do_destroy
   def do_create
-    system("adduser #{:name}")
+    system("adduser #{self.name} && adduser #{self.name} www-data")
+  end
+  def do_destroy
+    system("deluser #{self.name}")
   end
 end
