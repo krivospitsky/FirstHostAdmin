2889e3a49587e868c3eb4fdb3ca59c44f814d3d5
alex krivospitsky <alex@krivospitsky.ru>
2012-09-12 16:47:43 +0400

    наполняю

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ActiveRecord::Base
   before_create :do_create
   before_destroy :do_destroy
   def do_create
-    system("sudo", "adduser #{self.name}")
+    system("touch ~/1.txt")
+#    system("sudo", "adduser #{self.name}")
   end
   def do_destroy
     system("sudo deluser ", self.name)
