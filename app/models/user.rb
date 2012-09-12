39a1f6c587fdb967b15c5b79ab9ca94f9dcfc015
alex krivospitsky <alex@krivospitsky.ru>
2012-09-12 16:33:16 +0400

    наполняю

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,9 @@ class User < ActiveRecord::Base
   before_create :do_create
   before_destroy :do_destroy
   def do_create
-    system("adduser " . self.name . " && adduser " . self.name . " www-data")
+    system("sudo adduser " . self.name . " && adduser " . self.name . " www-data")
   end
   def do_destroy
-    system("deluser " . self.name)
+    system("sudo deluser " . self.name)
   end
 end
