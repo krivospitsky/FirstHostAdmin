722b7903bce9d6b3eda04030df2585075b729b98
alex krivospitsky <alex@krivospitsky.ru>
2012-09-14 11:35:21 +0400

    наполняю

--- chef-solo/solo.rb
+++ chef-solo/solo.rb
@@ -1,2 +1,2 @@
 file_cache_path "/var/chef-solo"
-cookbook_path ["/home/webmaster/projects/GirstHostAdmin/chef-solo/cookbooks"]
+cookbook_path ["/home/webmaster/projects/FirstHostAdmin/chef-solo/cookbooks"]
